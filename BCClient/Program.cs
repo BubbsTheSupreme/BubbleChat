04122e400fe53738c2933b34738c90f00f25ff9f
BubbsTheSupreme <trenton.mason14@outlook.com>
2023-05-14 09:06:00 -0400

    UI is dumb this is what Ive been using to test all the features and will probably stay until the very end when Im ready to do ui

--- BCClient/Program.cs
+++ BCClient/Program.cs
@@ -1,4 +1,8 @@
-﻿using BubbleChat.UI;
+﻿using System;
+using System.Threading;
+using BubbleChat.UI;
+using BubbleChat.Client;
+using BubbleChat.Packets;
 using BubbleChat.Logging;
 
 namespace BubbleChat;
@@ -8,6 +12,34 @@ class Program
 	static void Main(string[] args)
 	{
 		Logger.Init();
-		UserInterface ui = new();
+		BubbleChatClient client = new();
+		// UserInterface ui = new();
+		Console.Write("Username: ");
+		string username = Console.ReadLine();
+		Console.Write("Password: ");
+		string password = Console.ReadLine();
+
+		client.Connect(12345, "192.168.1.114");
+
+		Packet usernamePacket = new(0);
+		usernamePacket.WriteSecondId(0).WriteContent(username);
+		client.Send(usernamePacket.Finalize());
+
+		Thread.Sleep(500);
+
+		Packet passwordPacket = new(0);
+		passwordPacket.WriteSecondId(1).WriteContent(password);
+		client.Send(passwordPacket.Finalize());
+
+		Packet messagePacket;
+		string message;
+
+		while (client.IsConnected() == true)
+		{
+			message = Console.ReadLine();
+			messagePacket = new(1);
+			messagePacket.WriteSecondId(client.UserId).WriteContent(message);
+			client.Send(messagePacket.Finalize());
+		}
 	}
 }
